480c6738067223995c333033807b13deff6fe7c8
alireza babaie <60476541+babaie774@users.noreply.github.com>
2022-04-02 20:07:17 +0430

    Sting cheatsheet result

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
-import axios from "axios";
+const names = ['John', 'jack', 'Mary', 'frank', 'Bob'];
 
-const getData = async () => {
-    const { data } = await axios("https://api.coindesk.com/v1/bpi/currentprice.json");
-    return data
-}
+names.includes('Mary'); // true
+names.concat(['Sam', 'Tom']); // ['John', 'jack', 'Mary', 'frank', 'Bob', 'Sam', 'Tom']
+names.indexOf('Mary'); // 2
+names.join('-'); // 'John-jack-Mary-frank-Bob'
+names.pop(); // 'Bob'
+names.push('Sam'); // 5
+names.shift(); // 'John'
+names.unshift('Sam'); // 6
+names.slice(1, 3); // ['jack', 'Mary']
+names.splice(1, 2); // ['jack', 'Mary']
+names.reverse(); // ['Bob', 'frank', 'Mary', 'jack', 'John']
+names.sort(); // ['Bob', 'frank', 'John', 'jack', 'Mary']
+names.flat(); // [1, 2, 3, 4, 5, 'Bob', 'frank', 'John', 'jack', 'Mary']
+names.flat(2); // [1, 2, 3, 4, 5, 'John', 'jack', 'Mary', 'frank', 'Bob']
+names.values(); // ['John', 'jack', 'Mary', 'frank', 'Bob']
+names.keys(); // [0, 1, 2, 3, 4]
+names.entries(); // [[0, 'John'], [1, 'jack'], [2, 'Mary'], [3, 'frank'], [4, 'Bob']]
+names.toString(); // 'John,jack,Mary,frank,Bob'
+names.toLocaleString(); // 'John,jack,Mary,frank,Bob'
+names.toSource(); // '["John", "jack", "Mary", "frank", "Bob"]'
+names.log(); // 'John,jack,Mary,frank,Bob'
+names.valueOf(); // ['John', 'jack', 'Mary', 'frank', 'Bob']
+names.copyWithin(1, 3); // ['John', 'Mary', 'frank', 'Bob']
+names.fill('Sam', 1, 3); // ['John', 'Sam', 'Sam', 'Bob']
 
-getData().then((res) => {
-    console.log(res);
+names.find(function (name) {
+    return name.length > 3; // 'Mary'
 });
-console.log("ok");
+    
+names.some(function (name) {
+    return name.length > 3; // true
+});
 
+names.every(function (name) {
+    return name.length > 3; // false
+});
+
+names.forEach(function (name) {
+    console.log(name); // John, jack, Mary, frank, Bob
+});
+
+names.flatMap(function (name) { 
+    return name.split('');
+});
