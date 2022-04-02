0c415c54b60c76cb6fa02e3f8efdb48657341de5
alireza babaie <60476541+babaie774@users.noreply.github.com>
2022-04-02 11:35:30 +0430

    async-execise

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-const serverDate = new Promise((resolve, eject) => {
+const serverData = () => {
     const Data = {
         name: "alireza",
-        family: "babaie"
+        family: "babaei"
     }
-});
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("promise");
+            resolve(Data)
+        }, 500)
+
+        setTimeout(() => {
+            console.log("promise");
+            reject("rejected");
+        }, 1000)
+    })
+}
+
+const data = async () => {
+    return await serverData().then((res) => {
+        return res
+    }).catch((cat) => {
+        console.error(cat);
+    })
+}
+
+console.log(data());
+console.log("free");
