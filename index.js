2402d8056fbdc0d54b62e784bcc25999201324fb
alireza babaie <60476541+babaie774@users.noreply.github.com>
2022-04-10 00:03:17 +0430

    test-cupturing

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const fruit = ['apple', 'banana', 'orange', 'pear', 'strawberry'];
-
-const [apple, banana, ...rest] = fruit;
-
-console.log(rest);
+document.getElementById("div").addEventListener("click", () => {
+    console.log("div clicked");
+})
+document.getElementById("button").addEventListener("click", (e) => {
+    e.stopPropagation()
+    e.preventDefault();
+    console.log(e);
+    console.log(e.defaultPrevented);
+    console.log("button clicked");
+})
