00c12a0a89a267ca82d10b5ac00b51dd8367a821
alireza babaie <60476541+babaie774@users.noreply.github.com>
2022-04-11 11:26:20 +0430

    loop-exercise-1

--- index.js
+++ index.js
@@ -1,10 +1,46 @@
-document.getElementById("div").addEventListener("click", () => {
-    console.log("div clicked");
-})
-document.getElementById("button").addEventListener("click", (e) => {
-    e.stopPropagation()
-    e.preventDefault();
-    console.log(e);
-    console.log(e.defaultPrevented);
-    console.log("button clicked");
-})
+// proxy:
+// const user = {
+//     name: "alireza"
+// }
+
+// const userProxy = new Proxy(user, {
+//     get(target, pop) {
+//         return pop in target ? `my name is ${target[pop]}` : null; ;
+//     },
+// })
+
+// console.log(userProxy.name);
+// console.log(userProxy.family);
+
+
+//for:
+// const number =[1, 2, 3, 4, 5, 6];
+
+// for (var i = 0; i < number.length ; i++ ) {
+//     console.log(i);
+// }
+
+// Array(5).fill(0).map((item) => {
+//     console.log(item);
+// })
+
+
+//switch
+const operator = "+";
+const num1 = 1;
+const num2 = 3;
+
+switch (operator) {
+    case ("+"):
+        console.log(num1 + num2);
+        break;
+    case ("-"):
+        console.log(num1 - num2);
+        break;
+    case ("*"):
+        console.log(num1 * num2);
+        break;
+    case ("/"):
+        console.log(num1 / num2);
+        break;
+}
