c0f9aad11957bd412d2f8cc607e62ffc97e7fe86
alireza babaie <60476541+babaie774@users.noreply.github.com>
2022-04-17 08:39:01 +0430

    getter&Setter

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
-// for (var x = 0; x =< 5; x++) {
-//     console.log(x);
-// }
+const person = {
+    name: "alireza",
+    family: "babaie",
+    get fullName() {
+        return `${name} ${family}`
+    },
+    set fullName(value) {
+        if (value) {
+            [this.name, this.family] = value.splice('');
+        } else {
+            console.log("fullname sould have vlaue");
+        }
+    }
+}
+
+person.fullName = 'alireza babaei';
 
 
-// let i = 0
-// while (i < 5) {
-//     i++
-//     console.log(i)
-// }
-
-// let a = 0;
-
-// do {
-//     a++;
-//     console.log(a);
-// } while(a<5)
-
-// const myArray = Array(5).fill(0).map((_, index) => index)
-
-// console.log(myArray);
