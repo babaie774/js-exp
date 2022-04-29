d96a54498c3347d8de6c1d2f2443aa37b4d1ea33
alireza babaie <60476541+babaie774@users.noreply.github.com>
2022-04-29 13:04:11 +0430

    redirect-node-exercise

--- index.js
+++ index.js
@@ -2,20 +2,41 @@ const http = require("http");
 const qs = require("querystring");
 
 const server = http.createServer((req, res) => {
-    res.write(`
-    <html>
-    <head>
-    <title>search</title>
-    </head>
-    <body>
-    <form action="/" method="POST" >
-    <input name="search" />
-    <button type="submit">try it</button>
-    <form>
-    </body>
-    </html>
-    `)
-    res.end();
+    if (req.url == "/" && req.method == "GET") {
+        res.write(`
+        <html>
+        <head>
+        <title>search</title>
+        </head>
+        <body>
+        <form action="/" method="POST" >
+        <input name="search" type="search" />
+        <button type="submit">try it</button>
+        <form>
+        </body>
+        </html>
+        `)
+        res.end();
+    }
+
+    if (req.url == "/" && req.method == "POST") {
+        let body = "";
+        req.on("data", (data) => {
+            body += data
+        })
+
+        req.on("end", () => {
+            console.log(body);
+            const data = qs.parse(body)
+            console.log(data.search);
+            // res.statusCode = 302;
+            // res.setHeader('Location', '/');
+            // res.writeHead(302, { "Location": "/" })
+            res.writeHead(302, { 'Location': '/' })
+            res.end();
+        })
+        res.end();
+    }
 })
 
 server.listen(3001, "127.0.0.1", () => {
